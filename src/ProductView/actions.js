c1243a10d5f9bec491cef3314444be5063ed69c1
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-16 13:15:18 +1100

    touched up naming conventions in reudcers

--- src/ProductView/actions.js
+++ src/ProductView/actions.js
@@ -4,32 +4,32 @@ export const setMeta = (meta) => ({
     payload: meta
 });
 
-export const BREAK_DOWN_REQ = 'BREAK_DOWN_REQ';
+export const LOAD_BREAK_DOWN_IN_PROGRESS = 'LOAD_BREAK_DOWN_IN_PROGRESS';
 export const loadBreakDownInProgress = () => ({
-    type: BREAK_DOWN_REQ,
+    type: LOAD_BREAK_DOWN_IN_PROGRESS,
 });
-export const BREAK_DOWN_SUCCESS = 'BREAK_DOWN_SUCCESS';
+export const LOAD_BREAK_DOWN_SUCCESS = 'LOAD_BREAK_DOWN_SUCCESS';
 export const loadBreakDownSuccess = (data) => ({
-    type: BREAK_DOWN_SUCCESS,
+    type: LOAD_BREAK_DOWN_SUCCESS,
     payload: data
 });
-export const BREAK_DOWN_FAILURE = 'BREAK_DOWN_FAILURE';
+export const LOAD_BREAK_DOWN_FAILURE = 'LOAD_BREAK_DOWN_FAILURE';
 export const loadBreakDownFailure = () => ({
-    type: BREAK_DOWN_FAILURE
+    type: LOAD_BREAK_DOWN_FAILURE
 });
 
-export const BREAK_DOWN_OPTS_REQ = 'BREAK_DOWN_OPTS_REQ';
-export const breakDownOptsReq = () => ({
-    type: BREAK_DOWN_OPTS_REQ
+export const LOAD_BREAK_DOWN_OPTS_IN_PROGRESS = 'LOAD_BREAK_DOWN_OPTS_IN_PROGRESS';
+export const loadBreakDownOptsReq = () => ({
+    type: LOAD_BREAK_DOWN_OPTS_IN_PROGRESS
 });
 
-export const BREAK_DOWN_OPTS_SUCCESS = 'BREAK_DOWN_OPTS_SUCCESS';
-export const breakDownOptsSuccess = (opts) => ({
-    type: BREAK_DOWN_OPTS_SUCCESS,
+export const LOAD_BREAK_DOWN_OPTS_SUCCESS = 'LOAD_BREAK_DOWN_OPTS_SUCCESS';
+export const loadBreakDownOptsSuccess = (opts) => ({
+    type: LOAD_BREAK_DOWN_OPTS_SUCCESS,
     payload: opts
 });
 
-export const BREAK_DOWN_OPTS_FAILURE = 'BREAK_DOWN_OPTS_FAILURE';
-export const breakDownOptsFailure = () => ({
-    type: BREAK_DOWN_OPTS_FAILURE,
+export const LOAD_BREAK_DOWN_OPTS_FAILURE = 'LOAD_BREAK_DOWN_OPTS_FAILURE';
+export const loadBreakDownOptsFailure = () => ({
+    type: LOAD_BREAK_DOWN_OPTS_FAILURE,
 });
--- src/ProductView/reducers.js
+++ src/ProductView/reducers.js
@@ -1,28 +1,13 @@
 import {
-    META_DATA_REQ,
-    META_DATA_FAILURE,
-    META_DATA_SUCCESS,
-    BREAK_DOWN_REQ,
-    BREAK_DOWN_SUCCESS,
-    BREAK_DOWN_FAILURE,
-    BREAK_DOWN_OPTS_REQ,
-    BREAK_DOWN_OPTS_FAILURE,
-    BREAK_DOWN_OPTS_SUCCESS
+    SET_META,
+    LOAD_BREAK_DOWN_IN_PROGRESS,
+    LOAD_BREAK_DOWN_SUCCESS,
+    LOAD_BREAK_DOWN_FAILURE,
+    LOAD_BREAK_DOWN_OPTS_IN_PROGRESS,
+    LOAD_BREAK_DOWN_OPTS_FAILURE,
+    LOAD_BREAK_DOWN_OPTS_SUCCESS
 } from "./actions";
 
-const productMetaInitialState = {
-    isLoading: false,
-    data: {}
-}
-export const productMeta = (state = productMetaInitialState, action) => {
-    const {type, payload} = action;
-    switch (type) {
-        case META_DATA_REQ:
-        case META_DATA_FAILURE:
-        case META_DATA_SUCCESS:
-    }
-    return state
-}
 
 const breakDownInitialState = {
     isLoading: false,
@@ -31,9 +16,10 @@ const breakDownInitialState = {
 export const breakDown = (state = breakDownInitialState, action) => {
     const {type, payload} = action;
     switch (type) {
-        case BREAK_DOWN_REQ:
-        case BREAK_DOWN_SUCCESS:
-        case BREAK_DOWN_FAILURE:
+        case LOAD_BREAK_DOWN_IN_PROGRESS:
+        case LOAD_BREAK_DOWN_SUCCESS:
+        case LOAD_BREAK_DOWN_FAILURE:
+        case SET_META:
     }
     return state
 }
@@ -46,9 +32,9 @@ const breakDownOptsInitialState = {
 export const breakDownOpts = (state = breakDownInitialState, action) => {
     const {type, payload} = action;
     switch (type) {
-        case BREAK_DOWN_OPTS_REQ:
-        case BREAK_DOWN_OPTS_SUCCESS:
-        case BREAK_DOWN_OPTS_FAILURE:
+        case LOAD_BREAK_DOWN_OPTS_IN_PROGRESS:
+        case LOAD_BREAK_DOWN_OPTS_SUCCESS:
+        case LOAD_BREAK_DOWN_OPTS_FAILURE:
     }
     return state
 }
